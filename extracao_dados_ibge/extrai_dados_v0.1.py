b50590ab250324e551cccbd42eab3e6c722271e6
Alvaro <alcarnielo@gmail.com>
2020-11-29 05:01:21 -0300

    Carregar códigos do projéto de informações de saude suplementar

---
+++ extracao_dados_ibge/extrai_dados_v0.1.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  6 15:05:18 2020
+
+@author: alvaro
+
+Este código tem o objetivo de fazer download de dados do site do IBGE
+
+http://ftp.dadosabertos.ans.gov.br/FTP/PDA/informacoes_consolidadas_de_beneficiarios/
+
+
+----------------------------
+Versões:
+    /       -Versão original
+    0.1     - Modificação para não baixar dados do site e abrir direto o arquivo
+    
+Melhorias: 
+    -
+
+"""
+import requests as r
+from bs4 import BeautifulSoup as bs
+import os
+import pandas as pd
+
+## abrir a página e obter os links de cada arquivo que se deseja baixar
+
+
+def pega_complemento(link):
+    """
+    Esta função busca abre o link desejado da página de FTP do IBGE e retorna o
+complemento de acesso das paginas seguintes
+
+    Parameters
+    ----------
+    link :  - str
+            - Link da página que se quer extrair os complementos
+
+    Returns
+    -------
+    a_list: - list
+            - Lista de str contendo os complementos das páginas seguintes para 
+            navegação. Para acessar o complemento da página contido em a_list
+            deve-se:
+                a_list[x]['href']
+            tal que 'x' é a posição do elemento na lista
+    """
+    
+    site = r.get(link)
+
+    if site.status_code == r.codes.OK:
+        soup = bs(site.content, 'html.parser')
+        a_list = soup.find_all('a')
+    
+    return a_list
+  
+
+def download_arquivo(url, path):
+    """
+    Esta função baixa o arquivo do endereço desejado (url) e o salva no 
+    caminho de rede especificado em "path".
+    ref.:
+        https://pythoncafe.com.br/post/baixando-arquivos-da-internet/
+
+    Parameters
+    ----------
+    url :   - str
+            - endereço do elemento que se deseja baixar
+    path :  - str
+            - Caminho da rede no qual se deseja salvar o arquivo.
+
+    Returns
+    -------
+    None.
+
+    """
+    link = r.get(url)
+    if link.status_code == r.codes.OK:
+        with open(path, 'wb') as n_file:
+            print('Iniciando download ...')
+            n_file.write(link.content)
+        print('Download concluido!')
+        print('Arquivo salvo em: %s'%path)
+    else:
+        link.raise_for_status()
+    
+
+def abre_beneficiários(url):
+    """
+    Esta função abre direto o arquivo do site de informações consolidadas de 
+    beneficiários e abre como pandas um dataframe do pandas
+
+    link:
+        http://ftp.dadosabertos.ans.gov.br/FTP/PDA/informacoes_consolidadas_de_beneficiarios/
+        
+    Parameters
+    ----------
+    url :   - str
+            - Endereço do arquivo que será aberto
+    
+
+    Returns
+    -------
+    df:     - pandas.DataFrame()
+            - dataFrame do Pandas contendo os informes de beneficiários de planos 
+            de saúde
+
+    """
+    df = pd.read_csv(url, compression='zip', sep=';', encoding='iso-8859-1')
+    
+    return df
+
+def abre_proj_pop_(path):
+    """
+    Esta função abre o arquivo CSV contendo os municípios 
+    
+
+    Parameters
+    ----------
+    path :  - str
+            - Endereço do arquivo que será aberto
+
+
+    Returns
+    -------
+    df:     - pandas.DataFrame()
+            - dataFrame do Pandas contendo os informes de projeção populacional
+            por município do Brasil
+
+    """
+    df = pd.read_csv(path, sep=';', encoding='iso-8859-1', skiprows=3)
+    df['cod_ibge'] = df['Município'].str[:6]
+    df['nome_municipio'] = df['Município'].str[7:].tolist()
+    
+    return df
+    
+
+    
+def main(path):
+    """
+    Função principal que navega até a página que contém o link desejado a 
+partir das informações consolidads de benefíciários e baixa o conteúdo para um
+caminho desejado.
+
+    Parameters
+    ----------
+    path:   - str
+            - Caminho da rede onde deseja se salvar os arquivos baixados
+
+    Returns
+    -------
+    None.
+
+    """
+    link = 'http://ftp.dadosabertos.ans.gov.br/FTP/PDA/informacoes_consolidadas_de_beneficiarios/'
+    a_periodo = pega_complemento(link)
+    
+    for i in range(5,len(a_periodo)):
+        compl_1 = a_periodo[i]['href'] #pega o complemento do link
+        n_link = link + compl_1
+        
+        a_file = pega_complemento(n_link)
+        
+        # Criar a pasta para armazenar os arquivos contidos no link do comp_1
+        try:
+            os.mkdir(compl_1)
+        except FileExistsError:
+            print('FileExistsError: Pasta "%s" já existe!'%compl_1)
+            
+        # Navegar até a pasta em que serão armazenados os dados baixados
+        n_path = path+'/'+compl_1
+        os.chdir(n_path)
+        
+        for j in range(5,len(a_file)):
+            compl_2 = a_file[j]['href']
+            fl_link = n_link + compl_2
+            
+            #caminho da rede em que será salvo o novo arquivo
+            f_path = n_path+compl_2
+            download_arquivo(fl_link, f_path)
+        
+        # Retornar à pasta raiz "path"
+        os.chdir(path)
+            
+    
+if __name__ == '__main__':
+    path = '/home/alvaro/Documentos/Alvaro/Estudos_Python/BootCamp_Alura/extracao_dados_ibge'
+    main(path)
+    
+    
---
+++ extracao_dados_ibge/manipula_dados.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 29 03:36:56 2020
+
+Este programa abre os dados de informe de beneficiarios de planos de saude de
+várias cidades dentro dos estados do Brasil e correlaciona com a população e 
+faixa etária delas
+
+fontes:
+    - informe de planos de saúde
+http://ftp.dadosabertos.ans.gov.br/FTP/PDA/informacoes_consolidadas_de_beneficiarios/
+
+
+
+@author: alvaro
+"""
+
+import pnadas as pd
